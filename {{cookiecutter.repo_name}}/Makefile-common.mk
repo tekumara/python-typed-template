e3016160d0045e5d6f0d16f367f2cd9582bd1261
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2023-03-15 15:20:59 +1100

    chore: bump deps

--- {{cookiecutter.repo_name}}/Makefile-common.mk
+++ {{cookiecutter.repo_name}}/Makefile-common.mk
@@ -14,7 +14,7 @@ $(pip):
 # create venv using system python even when another venv is active
 	PATH=$${PATH#$${VIRTUAL_ENV}/bin:} python3 -m venv --clear $(venv)
 	$(venv)/bin/python --version
-	$(pip) install pip~=22.3 wheel~=0.37
+	$(pip) install pip~=23.0 wheel~=0.40
 
 $(venv): pyproject.toml $(pip)
 	$(pip) install -e '.[dev]'
