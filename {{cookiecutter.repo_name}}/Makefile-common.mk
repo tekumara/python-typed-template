156759ab24a3f22a8992b90f3f15a49538d9170e
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2023-08-26 17:06:05 +1000

    feat: initialise hooks environment during make install
    This now happens on `make install` rather than when first running `make hooks`:
    ```
    .venv/bin/pre-commit install-hooks
    [INFO] Installing environment for https://github.com/pre-commit/pre-commit-hooks.
    [INFO] Once installed this environment will be reused.
    [INFO] This may take a few minutes...
    ```
    Which is useful when `make install` is cached (eg: in a docker layer).

--- {{cookiecutter.repo_name}}/Makefile-common.mk
+++ {{cookiecutter.repo_name}}/Makefile-common.mk
@@ -60,11 +60,7 @@ publish: $(venv)
 hooks: node_modules $(venv)
 	$(venv)/bin/pre-commit run --show-diff-on-failure --color=always --all-files --hook-stage push
 
-install-hooks: .git/hooks/pre-commit .git/hooks/pre-push
-	$(venv)/bin/pre-commit install-hooks
-	
-.git/hooks/pre-commit: $(venv)
-	$(venv)/bin/pre-commit install -t pre-commit
+install-hooks: .git/hooks/pre-push
 
 .git/hooks/pre-push: $(venv)
-	$(venv)/bin/pre-commit install -t pre-push
+	$(venv)/bin/pre-commit install --install-hooks -t pre-push
