0aeb5498e243ba4e6b162c574a2f8edca131daa2
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2025-04-19 13:33:26 +1000

    refactor: remove $(venv) target
    to simplify, given uv run and uv sync will (re)create the venv when pyproject.toml & .python-version change

--- snapshots/default/Makefile-common.mk
+++ snapshots/default/Makefile-common.mk
@@ -1,29 +1,25 @@
-MAKEFLAGS += --warn-undefined-variables --check-symlink-times
+MAKEFLAGS += --warn-undefined-variables
 SHELL = /bin/bash -o pipefail
 .DEFAULT_GOAL := help
-.PHONY: help clean install format check pyright test dist hooks install-hooks
+.PHONY: help clean install format check pyright test dist hooks install-hooks .uv
 
 ## display help message
 help:
 	@awk '/^##.*$$/,/^[~\/\.0-9a-zA-Z_-]+:/' $(MAKEFILE_LIST) | awk '!(NR%2){print $$0p}{p=$$0}' | awk 'BEGIN {FS = ":.*?##"}; {printf "\033[36m%-20s\033[0m %s\n", $$1, $$2}' | sort
 
-venv ?= .venv
-# this is a symlink so we set the --check-symlink-times makeflag above
-python := $(venv)/bin/python
+## check that uv is installed
+.uv:
+	@uv --version || { echo 'Please install uv: https://docs.astral.sh/uv/getting-started/installation/' && exit 13 ;}
 
-$(python): $(if $(value CI),|,) .python-version
-	uv venv
-
-$(venv): $(if $(value CI),|,) pyproject.toml $(python)
+.sync:
 	uv sync
-	touch $(venv)
 
 # delete the venv
 clean:
-	rm -rf $(venv)
+	rm -rf .venv
 
 ## create venv and install this package and hooks
-install: $(venv) $(if $(value CI),,install-hooks)
+install: .uv .sync $(if $(value CI),,install-hooks)
 
 ## format, lint and type check
 check: export SKIP=test
@@ -34,28 +30,28 @@ format: export SKIP=pyright,test
 format: hooks
 
 ## pyright type check
-pyright: $(venv)
+pyright:
 	PYRIGHT_PYTHON_IGNORE_WARNINGS=1 uv run pyright
 
 ## run tests
-test: $(venv)
+test:
 	uv run pytest
 
 ## build python distribution
-dist: $(venv)
+dist:
 # start with a clean slate (see setuptools/#2347)
 	rm -rf build dist *.egg-info
 	uv run -m build --wheel
 
 ## publish to pypi
-publish: $(venv)
+publish:
 	uv run twine upload dist/*
 
 ## run pre-commit git hooks on all files
-hooks: $(venv)
+hooks:
 	uv run pre-commit run --color=always --all-files --hook-stage push
 
 install-hooks: .git/hooks/pre-push
 
-.git/hooks/pre-push: $(venv)
+.git/hooks/pre-push:
 	uv run pre-commit install --install-hooks -t pre-push
